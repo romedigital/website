b3e4f77f5b2c71e90f741d715e0f1a6de5473708
William Bojczuk <williambojczuk@gmail.com>
2024-04-10 11:16:41 -0400

    [A[Bupdate

--- src/app/(mainsite)/components/onboardingpage/Payment/Payment.tsx
+++ src/app/(mainsite)/components/onboardingpage/Payment/Payment.tsx
@@ -16,6 +16,18 @@ if(typeof window !== "undefined"){
   let buttonHTML = ""
 
   switch(pkg){
+    case("business"):
+      buttonHTML = `<script async
+      src="https://js.stripe.com/v3/buy-button.js">
+    </script>
+    
+    <stripe-buy-button
+      buy-button-id="buy_btn_1P42vsKaTewv8x5BarPIeHSm"
+      publishable-key="pk_live_51OTXVsKaTewv8x5B7ocCPmnBDkWU0v37izsVOBOTKx8a457McIMV4KP7TeAzxabBumxqtThWW8N82jKOkPqRKXdi00efBOjxag"
+    >
+    </stripe-buy-button>`
+    break;
+    
     case("standard"):
       buttonHTML = `<script async
       src="https://js.stripe.com/v3/buy-button.js">
