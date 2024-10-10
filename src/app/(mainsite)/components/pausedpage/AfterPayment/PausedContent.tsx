daadbb8f59eed6c5dd78fcd570d7fcafbfd207c9
William Bojczuk <williambojczuk@gmail.com>
2024-10-09 21:54:25 -0400

    updateddd

--- src/app/(mainsite)/components/pausedpage/AfterPayment/PausedContent.tsx
+++ src/app/(mainsite)/components/pausedpage/AfterPayment/PausedContent.tsx
@@ -10,7 +10,7 @@ export default function PausedContent() {
         <div className={styles.contentWrapper}>
         <h1 className={styles.title}>This website has been paused...</h1>
 
-        <p className={styles.content}>If you are an admin, contact Rome Digital to get this website re-instated.
+        <p className={styles.content}>If you are an admin, contact us to get this website re-instated.
         </p>
         <img src="/img/colosseum.webp" alt="" className="bg-img" />
     <div style={{backgroundColor: "rgba(255,255,255,0.95)"}} className="shader"></div>
