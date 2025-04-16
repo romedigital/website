f36e914e4cd730e0b9e20a74b3c332997aa6cfc0
William Bojczuk <williambojczuk@gmail.com>
2025-04-15 21:04:07 -0400

    updated

--- src/app/(mainsite)/components/onboardingpage/Payment/Payment.tsx
+++ src/app/(mainsite)/components/onboardingpage/Payment/Payment.tsx
@@ -91,7 +91,7 @@ if(typeof window !== "undefined"){
         <div className={styles.contentWrapper}>
         <h1 className={styles.title}>Almost There{businessName}!</h1>
 
-        <p className={styles.content}>For us to be able to begin designing your website, all there is to do is to complete your subscription at the secure link below. If you aren't fully satisfied with the initial design, we will refund you 100% of your money, so there's absolutely no risk!
+        <p className={styles.content}>For us to be able to begin designing your website, all there is to do is to complete your subscription at the secure link below. If you aren't fully satisfied with the initial design, we will refund you 100% of your money, so there's absolutely no risk! We support all major cards and banks!
         </p>
         <div className={styles.buttonWrapper} dangerouslySetInnerHTML={{__html: buttonHTML}}>
 
