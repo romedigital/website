dd4a014eee40e637cb6c813c0df7de6a91c859cd
William Bojczuk <williambojczuk@gmail.com>
2025-06-17 07:22:23 -0400

    updated old link and removed addy

--- src/app/(mainsite)/components/misc/Footer/Footer.tsx
+++ src/app/(mainsite)/components/misc/Footer/Footer.tsx
@@ -56,7 +56,7 @@ export default function Footer() {
                 
                 <aside className={`${styles.contactItem} ${styles.listItem}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"></path></svg>
-                  <a href="https://google.com" target="_blank">2200, E. Hermitage RD Kingston GA</a>
+                  <a href="https://google.com" target="_blank">Rome, GA</a>
                 </aside>
 
               </ul>
@@ -65,7 +65,7 @@ export default function Footer() {
 
       </div>
 
-      <div className={styles.copyright}>© Copyright 2023 to Present - <a href="https://www.romesites.co" target="_blank">Rome Sites</a></div>
+      <div className={styles.copyright}>© Copyright 2025 to Present - <a href="https://www.romesites.co" target="_blank">Rome Sites</a></div>
         
        
     </footer>
