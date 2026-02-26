cd2c3bb1661a15a212644b7944559e323d9eb4c3
William Bojczuk <williambojczuk@gmail.com>
2026-02-26 17:46:59 -0500

    GOD

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import styles from "./contact.module.css"
 import Header from "@/app/(mainsite)/components/contactpage/Header/Header"
 import ContactInfo from "@/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo"
-import ContactForm from "@/app/(mainsite)/components/contactpage/ContactForm/ContactForm"
 import Image from "next/image"
 import ContentCalendar from "@/app/(mainsite)/components/contactpage/ContactCalendar/ContentCalendar"
 
@@ -16,8 +15,6 @@ export default function ContactPage(){
         <main className={styles.main}>
             <Header />
             <ContactInfo />
-            {/* <ContentCalendar /> */}
-            {/* <ContactForm /> */}
 
             <Image className={styles.mainBg} src="/img/contact-bg.png" width={1024} height={1024} alt="Contact background image" />
         </main>
